6e446d1699fd8313a1a65a8b87997ebea0a1b78c
Braxton <braxtongjohnston@gmail.com>
2021-06-27 21:26:04 -0600

    Update __init__.py
    sys was unnecessary

--- __init__.py
+++ __init__.py
@@ -2,6 +2,5 @@
 Set up developer logging using the environment variable PYLOGPRIM_DEV.  May be overkill for such a small code base but why not.
 """
 import os
-import sys
 
 _DEV = True if 'PYLOGPRIM_DEV' in os.environ else False
