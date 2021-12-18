ba17d960187e49f98baa4034ee93ca8ba60a4f3e
braxtonj <braxtongjohnston@gmail.com>
2021-12-17 22:02:57 -0700

    Update SamplingClasses.py

--- SamplingClasses.py
+++ SamplingClasses.py
@@ -8,7 +8,7 @@ class Sampling:
     """
     Sampling class boilerplate that returns True no matter what.
 
-    sendCheck is required.  It is the deciding interface.
+    sendCheck is required.  It is the "decision interface".
     """
 
     def __init__(self): pass
@@ -25,19 +25,18 @@ class UniformSampling:
     Uniform distribution
     """
 
-    def __init__(self, send_drop_ratio=1.0):
+    def __init__(self, send_ratio=1.0):
         """
-        send_drop_ratio: percentage of logs to let through.  Defaults to always send
+        send_ratio: percentage of logs to let through.  Defaults to always send
         """
-        if send_drop_ratio > 1.0 or send_drop_ratio < 0.0:
-            if _DEV: _L.info('{"message":"UniformSampling.send_drop_ratio must be between 0 and 1. {} is invalid, setting to 1.0"}'.format(send_drop_ratio))
-            send_drop_ratio = 1.0
-        self.send_drop_ratio = send_drop_ratio
+        if send_ratio > 1.0 or send_ratio < 0.0:
+            if _DEV: _L.info('{"message":"UniformSampling.send_ratio must be between 0 and 1. {} is invalid, setting to 1.0"}'.format(send_ratio))
+            send_ratio = 1.0 # set to 1.0 if out of bounds: [0,1]
+        self.send_ratio = send_ratio
 
     def sendCheck(self):
         """
-        Binary send decision based on a uniform distribution.  If number is less than send_drop_ratio, send it
+        Binary send decision based on a uniform distribution.
         """
-        if random.uniform(0.0,1.0) < self.send_drop_ratio:
-            return True
-        return False
+        # Send if random number is less than send_ratio
+        return True if random.uniform(0.0,1.0) < self.send_ratio else False
